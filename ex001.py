ba71c675cd04f0adfdbcf5d3f11f675d773ef088
Guilherme Braz <7sbraz777@gmail.com>
2026-04-12 10:23:38 -0300

    ex001

---
+++ ex001.py
@@ -0,0 +1,20 @@
+'''Faça um programa que receba a quantidade de m3 de água consumidos e o custo por litro de água e calcule:
+
+a) O valor a ser pago pela água; 
+
+b) O valor a ser pago pelo esgoto, sabendo que este corresponde a 80% do valor da água consumida;
+
+c) O valor total da conta, considerando água e esgoto.
+
+Dica: Lembre-se que 1 m3 = 1.000 litros'''
+entrada = float(input())
+valores = entrada.split()
+metro3 = valores[0]
+val_litro = valores[1]
+tot_litros = metro3 * 1000
+val_agua = tot_litros * val_litro
+esgoto = val_agua * 0.80
+total = val_agua + esgoto
+print (f'{val_agua:.2f}')
+print (f'{esgoto:.2f}')
+print (f'{total:.2f}')
